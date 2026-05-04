0375cbdb9e8bae0b68b42c12f2dc70af439f9cce
north238 <i.make.a.studious.effort@gmail.com>
2026-05-04 19:55:27 +0000

    feat: 環境変数の呼び出し

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,38 @@
 package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	APIKey  string
+	Model   string
+	Timeout int
+}
+
+func NewConfig() (*Config, error) {
+	apikey := os.Getenv("ANTHROPIC_API_KEY")
+	if apikey == "" {
+		return nil, fmt.Errorf("invalied to APIKey")
+	}
+
+	model := os.Getenv("LRV_MODEL")
+	if model == "" {
+		model = "claude-sonnet-4-5"
+	}
+
+	timeout, err := strconv.Atoi(os.Getenv("LRV_TIMEOUT"))
+	if err != nil {
+		timeout = 120
+	}
+
+	config := &Config{
+		APIKey:  apikey,
+		Model:   model,
+		Timeout: timeout,
+	}
+
+	return config, nil
+}
