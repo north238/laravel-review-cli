a82f21d7daedea6cf2391ce271b0487109c9da5c
north238 <i.make.a.studious.effort@gmail.com>
2026-06-19 01:23:46 +0000

    feat: 失敗観点のサマリー出力

--- internal/output/markdown.go
+++ internal/output/markdown.go
@@ -11,11 +11,20 @@ import (
 type MarkdownFormatter struct{}
 
 func (f *MarkdownFormatter) Format(w io.Writer, result *review.AggregatedResult) error {
-	// ファイルパスのマップを定義
+	// マップを定義
 	filePathMapping := make(map[string][]review.Finding)
+	failedAspects := make([]review.Aspect, 0)
+
+	// 失敗観点をまとめる
+	for _, r := range result.Results {
+		if r.Error != nil {
+			failedAspects = append(failedAspects, r.Aspect)
+		}
+	}
 
 	// ファイルパスでまとめる
 	for _, result := range result.Results {
+		// Findingsをループ
 		for _, finding := range result.Findings {
 			filePathMapping[finding.File] = append(filePathMapping[finding.File], finding)
 		}
@@ -40,6 +49,27 @@ func (f *MarkdownFormatter) Format(w io.Writer, result *review.AggregatedResult)
 			if err != nil {
 				return fmt.Errorf("【ERROR】failed to output markdown: %w", err)
 			}
+		}
+
+	}
+
+	// 失敗観点情報を書き出し
+	if len(failedAspects) != 0 {
+		_, err := fmt.Fprintf(w, "### 失敗観点情報\n\n")
+		if err != nil {
+			return fmt.Errorf("【ERROR】failed to output markdown: %w", err)
+		}
+
+		// 失敗観点ループ
+		for _, failedAspect := range failedAspects {
+			_, err := fmt.Fprintf(w, "- %s\n", failedAspect)
+			if err != nil {
+				return fmt.Errorf("【ERROR】failed to output markdown: %w", err)
+			}
+		}
+		_, err = fmt.Fprintf(w, "\n\n")
+		if err != nil {
+			return fmt.Errorf("【ERROR】failed to output markdown: %w", err)
 		}
 	}
 
